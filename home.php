b34b54ed8f4478fee6017cceb79a0abc8efbcb75
Renan <renanwitt@unochapeco.edu.br>
2018-10-15 22:36:10 -0300

    1.0

--- home.php
+++ home.php
@@ -1,1 +1,21 @@
-SISTEMA
+<html>
+	<head>
+		<title>Market Prices | Brazil</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="./css/estilos-home.css"/>
+	</head>
+	<body>
+		<!-- Início do topo -->
+		<div class="topo">
+			<!-- Início do logo do menu -->
+			<div class="logo-menu">
+				<br><img src="./img/logo-menu.png"/>
+			</div>
+			<!-- Fim do logo do menu -->
+			<br><div class="login">
+					<form class="botao-login" method="get" action="cadastro.php"><button type="submit">LOGIN</button></form>
+				</div>
+		</div>
+		<!-- Fim do topo -->
+	</body>
+</html>
