75525802e491e3e8f213cdeafd5849e95e23c704
Renan <renanwitt@unochapeco.edu.br>
2018-10-15 21:13:20 -0300

    Xamp

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php ?>
+
+<html>
+	<head>
+		<title>Market Prices | Brazil</title>
+		<meta charset="UTF-8" />
+		<!-- <meta http-equiv="refresh" content="4; url=home.php"> -->
+		<link rel="icon" href="img/icon.ico" type="image/x-icon" />
+		<link rel="stylesheet" href="css/estilos.css"/>
+		<link rel="stylesheet" href="css/progress-bar.css"/>
+	</head>
+	<body>
+		<!-- Início do logo -->
+		<div class="logo">
+			<br><br><br><br><br><br><br><br><br><br><br>
+			<img src="img/logo-grande.png"/>
+		</div>
+		<!-- Fim do logo -->
+		<!-- Início da barra de progresso -->
+		<br><br><br><br><br><br><br><br>
+		<div class="progress">
+			<div class="progress-bar"></div>
+		</div>
+		<!-- Fim da barra de progresso -->
+	</body>
+</html>
